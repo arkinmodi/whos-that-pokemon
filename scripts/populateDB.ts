f7f2f3f3c3df6003d36a9535e0d28a1b1e35463a
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2022-07-18 09:06:22 -0400

    create script to populate db

---
+++ scripts/populateDB.ts
@@ -0,0 +1,35 @@
+import { PokemonClient } from "pokenode-ts";
+
+import { prisma } from "../src/server/db/client";
+import { MAX_NATIONAL_DEX_ID } from "../src/utils/pokemonService";
+
+const fillPokemonData = async () => {
+  const pokemonApiClient = new PokemonClient();
+  const pokemonDataList = pokemonApiClient.listPokemons(0, MAX_NATIONAL_DEX_ID);
+
+  const pokemonData = (await pokemonDataList).results.map((pokemon, index) => ({
+    id: index + 1,
+    name: pokemon.name,
+    spriteUrl: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
+      index + 1
+    }.png`,
+  }));
+
+  // Clear any existing data
+  await prisma.pokemon.deleteMany({});
+
+  const result = await prisma.pokemon.createMany({
+    data: pokemonData
+  });
+  console.log("Pokemon created?", result);
+
+  // createMany is not supported for SQLite
+  // pokemonData.forEach(async (pokemon) => {
+  //   const result = await prisma.pokemon.create({ data: pokemon });
+  //   console.log("Pokemon %s created?", pokemon.id, result);
+  // });
+};
+
+fillPokemonData();
+
+// NODE_ENV=development npx ts-node --compiler-options '{"module":"commonjs"}' scripts/populateDB.ts
