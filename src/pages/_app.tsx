feb617acef9303c8ba385fdb6ea5389d8017263a
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2022-07-21 07:55:04 -0400

    add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,9 +4,42 @@ import type { AppRouter } from "../server/router";
 import type { AppType } from "next/dist/shared/lib/utils";
 import superjson from "superjson";
 import "../styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  const description =
+    "A cheap clone of the classic Pokémon Ad break, Who's That Pokémon!";
+  const title = "Who's That Pokémon";
+
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <link rel="icon" href={"/favicon.ico"} />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
 const getBaseUrl = () => {
