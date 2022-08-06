f530248a0fb73ee23910f6fe8291d8967c01b81c
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2022-08-06 15:17:09 -0400

    update docker compose to use mysql

--- prisma/migrations/20220718130403_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Pokemon" (
-    "id" INTEGER NOT NULL,
-    "name" TEXT NOT NULL,
-    "spriteUrl" TEXT NOT NULL,
-
-    CONSTRAINT "Pokemon_pkey" PRIMARY KEY ("id")
-);
