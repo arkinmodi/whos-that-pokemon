7be26afb1f1ea9df07c148e8096efe2ce45a3a5f
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2022-10-26 14:43:33 -0400

    feat: switch to local sqlite db

--- scripts/populateDB.ts
+++ scripts/populateDB.ts
@@ -22,16 +22,16 @@ const fillPokemonData = async () => {
   // Clear any existing data
   await prisma.pokemon.deleteMany({});
 
-  const result = await prisma.pokemon.createMany({
-    data: pokemonData,
-  });
-  console.log("Pokemon created?", result);
+  // const result = await prisma.pokemon.createMany({
+  //   data: pokemonData,
+  // });
+  // console.log("Pokemon created?", result);
 
   // createMany is not supported for SQLite
-  // pokemonData.forEach(async (pokemon) => {
-  //   const result = await prisma.pokemon.create({ data: pokemon });
-  //   console.log("Pokemon %s created?", pokemon.id, result);
-  // });
+  pokemonData.forEach(async (pokemon) => {
+    const result = await prisma.pokemon.create({ data: pokemon });
+    console.log("Pokemon %s created?", pokemon.id, result);
+  });
 };
 
 fillPokemonData();
