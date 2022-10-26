34d64e8a39812ea9688f7b31c5303e676c7a0ca9
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2022-10-26 15:46:41 -0400

    feat: switch to local json db (#2)

--- scripts/populateDB.ts
+++ scripts/populateDB.ts
@@ -1,37 +1,22 @@
-import { PokemonClient } from "pokenode-ts";
-
 import { prisma } from "../src/server/db/client";
-import { MAX_NATIONAL_DEX_ID } from "../src/server/router/pokemon";
+import { getPokemonData } from "./getPokemonData";
 
 const fillPokemonData = async () => {
-  const pokemonApiClient = new PokemonClient();
-  const pokemonDataList = pokemonApiClient.listPokemons(0, MAX_NATIONAL_DEX_ID);
-
-  const pokemonData = (await pokemonDataList).results.map((pokemon, index) => ({
-    id: index + 1,
-    name: pokemon.name
-      .toLowerCase()
-      .split("-")
-      .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
-      .join("-"),
-    spriteUrl: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
-      index + 1
-    }.png`,
-  }));
+  const pokemonData = await getPokemonData();
 
   // Clear any existing data
   await prisma.pokemon.deleteMany({});
 
-  const result = await prisma.pokemon.createMany({
-    data: pokemonData,
-  });
-  console.log("Pokemon created?", result);
+  // const result = await prisma.pokemon.createMany({
+  //   data: pokemonData,
+  // });
+  // console.log("Pokemon created?", result);
 
   // createMany is not supported for SQLite
-  // pokemonData.forEach(async (pokemon) => {
-  //   const result = await prisma.pokemon.create({ data: pokemon });
-  //   console.log("Pokemon %s created?", pokemon.id, result);
-  // });
+  pokemonData.forEach(async (pokemon) => {
+    const result = await prisma.pokemon.create({ data: pokemon });
+    console.log("Pokemon %s created?", pokemon.id, result);
+  });
 };
 
 fillPokemonData();
